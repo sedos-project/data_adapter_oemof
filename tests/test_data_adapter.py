a66092e47fc7cda1d4d7f7e2d9810fcf8c8e84f5
jnnr <32454596+jnnr@users.noreply.github.com>
2022-12-13 15:26:55 +0100

    Add assertion to test

--- tests/test_data_adapter.py
+++ tests/test_data_adapter.py
@@ -1,16 +1,21 @@
+import pathlib
 import pandas as pd
 
 from data_adapter_oemof import adapter_classes
 
 
+TEST_DIR = pathlib.Path(__file__).parent
+TEMP_DIR = TEST_DIR / "_temp"
+
+
 def test_adapter():
-    regions = ["B", "BB"]
-    carriers = ["wind", "pv"]
+    regions = ["B"]
+    carriers = ["wind"]
     techs = ["onshore"]
     types = ["volatile"]
-    adapters = [adapter_classes.ADAPTER_MAP[type] for type in types]
+    adapters = {type: adapter_classes.ADAPTER_MAP[type] for type in types}
 
-    for adapter in adapters:
+    for type, adapter in adapters.items():
         instances = []
         for region in regions:
             for carrier in carriers:
@@ -18,8 +23,16 @@ def test_adapter():
                     instances.append(adapter.parametrize())
 
         df = pd.DataFrame(instances)
-        print(df)
 
+        path_default = (
+            TEST_DIR
+            / "_files"
+            / "tabular_datapackage_mininmal_example"
+            / "data"
+            / "elements"
+            / f"{type}.csv"
+        )
 
-if __name__ == "__main__":
-    test_adapter()
+        df_default = pd.read_csv(path_default, sep=";")
+
+        pd.testing.assert_frame_equal(df, df_default)
