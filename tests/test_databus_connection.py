3042d98a7a0aff929e4d98f26888e1435a5a4813
jnnr <32454596+jnnr@users.noreply.github.com>
2022-12-13 10:51:26 +0100

    Create folder to save collection

--- tests/test_databus_connection.py
+++ tests/test_databus_connection.py
@@ -1,10 +1,14 @@
-from data_adapter import download_collection
+import os
+
+from data_adapter.databus import download_collection
 from data_adapter_oemof import settings
 
 
 def test_download_collection():
     collection_url = "https://energy.databus.dbpedia.org/felixmaur/collections/modex_test_renewable"
     collection_output_directory=settings.COLLECTIONS_DIR
+    if not os.path.exists(collection_output_directory):
+        os.makedirs(collection_output_directory)
     download_collection(
         collection_url=collection_url,
         collection_output_directory=collection_output_directory,
