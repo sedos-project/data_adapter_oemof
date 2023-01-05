87395bba312504187631baed979b8f25acc1a303
jnnr <32454596+jnnr@users.noreply.github.com>
2023-01-05 17:13:34 +0100

    Update test

--- tests/test_data_adapter.py
+++ tests/test_data_adapter.py
@@ -1,38 +1,54 @@
 import pathlib
+from collections import namedtuple
+
 import pandas as pd
 
-from data_adapter_oemof import adapter_classes
-
+from data_adapter_oemof import adapters
 
 TEST_DIR = pathlib.Path(__file__).parent
 TEMP_DIR = TEST_DIR / "_temp"
 
 
 def test_adapter():
-    regions = ["B"]
-    carriers = ["wind"]
-    techs = ["onshore"]
-    types = ["volatile"]
-    adapters = {type: adapter_classes.ADAPTER_MAP[type] for type in types}
+    Component = namedtuple("Component", ["region", "carrier", "tech", "type"])
 
-    for type, adapter in adapters.items():
-        instances = []
-        for region in regions:
-            for carrier in carriers:
-                for tech in techs:
-                    instances.append(adapter.parametrize())
+    # first, just a list of components
+    components = [
+        Component(region="B", carrier="wind", tech=tech, type="volatile")
+        for tech in ["onshore", "offshore"]
+    ]
 
-        df = pd.DataFrame(instances)
+    # collect instances of the adapters
+    type_adapters = {}
+    for component in components:
+        if component.type not in type_adapters:
+            type_adapters[component.type] = []
 
-        path_default = (
-            TEST_DIR
-            / "_files"
-            / "tabular_datapackage_mininmal_example"
-            / "data"
-            / "elements"
-            / f"{type}.csv"
-        )
+        adapter = adapters.ADAPTER_MAP[component.type]
+        type_adapters[component.type].append(adapter)
 
-        df_default = pd.read_csv(path_default, sep=";")
+    # parametrize
+    for type in type_adapters:
+        type_adapters[type] = [adapter.parametrize() for adapter in type_adapters[type]]
 
-        pd.testing.assert_frame_equal(df, df_default)
+    # create a dictionary of dataframes
+    dataframes = {
+        type: pd.DataFrame(adapters) for type, adapters in type_adapters.items()
+    }
+
+    df = dataframes["volatile"]
+
+    path_default = (
+        TEST_DIR
+        / "_files"
+        / "tabular_datapackage_mininmal_example"
+        / "data"
+        / "elements"
+        / "volatile.csv"
+    )
+
+    df_default = pd.read_csv(path_default, sep=";")
+
+    assert set(df.columns) == set(df_default.columns)
+
+    pd.testing.assert_frame_equal(df, df_default)
