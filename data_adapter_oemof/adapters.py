83997bbd6a51bc5de4cde90dd91084a7616aee98
jnnr <32454596+jnnr@users.noreply.github.com>
2023-01-06 11:30:01 +0100

    Revise adapters

--- data_adapter_oemof/adapters.py
+++ data_adapter_oemof/adapters.py
@@ -1,20 +1,70 @@
+from data_adapter_oemof.dataclasses import (
+    Commodity,
+    Conversion,
+    Load,
+    Storage,
+    Volatile,
+)
+
+
 class Adapter:
-    def __init__(self, datacls, data=None):
+    def __init__(self, datacls, data=None, builders=None):
+        if builders is None:
+            builders = BUILDER_MAP
+
         self.datacls = datacls
         self.data = data
+        self.builders = builders
 
     def parametrize_dataclass(self):
-        instance = self.datacls(
-            name="calculations.get_name(data.region, data.carrier, data.tech)",
-            type="self.data.type",
-            carrier="carrier",
-            tech="",
-            capacity=8,  # get_param("capacity"),
-            capacity_cost=8,  # get_capacity_cost("capacity_cost"),
-            bus=8,  # get_param("bus"),
-            marginal_cost=8,  # get_param("marginal_cost"),
-            profile=8,  # None,
-            output_parameters=8,  # None,
-        )
+
+        build_element = self.builders[self.datacls]
+
+        instance = build_element(self)
 
         return instance
+
+
+def build_commodity(self):
+    instance = self.datacls()
+    return instance
+
+
+def build_conversion(self):
+    instance = self.datacls()
+    return instance
+
+
+def build_load(self):
+    instance = self.datacls()
+    return instance
+
+
+def build_storage(self):
+    instance = self.datacls()
+    return instance
+
+
+def build_volatile(self):
+    instance = self.datacls(
+        name="calculations.get_name(data.region, data.carrier, data.tech)",
+        type="self.data.type",
+        carrier="carrier",
+        tech="",
+        capacity=8,  # get_param("capacity"),
+        capacity_cost=8,  # get_capacity_cost("capacity_cost"),
+        bus=8,  # get_param("bus"),
+        marginal_cost=8,  # get_param("marginal_cost"),
+        profile=8,  # None,
+        output_parameters=8,  # None,
+    )
+    return instance
+
+
+BUILDER_MAP = {
+    Commodity: build_commodity,
+    Conversion: build_conversion,
+    Load: build_load,
+    Storage: build_storage,
+    Volatile: build_volatile,
+}
