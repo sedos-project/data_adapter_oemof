0baa153702bb333f8423f5ebdb558be0494b9938
jnnr <32454596+jnnr@users.noreply.github.com>
2023-01-23 15:49:38 +0100

    Adapt test to changed api

--- tests/test_databus_connection.py
+++ tests/test_databus_connection.py
@@ -1,15 +1,11 @@
-import os
-
 from data_adapter.databus import download_collection
-from data_adapter_oemof import settings
 
 
 def test_download_collection():
-    collection_url = "https://energy.databus.dbpedia.org/felixmaur/collections/modex_test_renewable"
-    collection_output_directory=settings.COLLECTIONS_DIR
-    if not os.path.exists(collection_output_directory):
-        os.makedirs(collection_output_directory)
+    collection_url = (
+        "https://energy.databus.dbpedia.org/felixmaur/collections/modex_test_renewable"
+    )
+
     download_collection(
         collection_url=collection_url,
-        collection_output_directory=collection_output_directory,
     )
