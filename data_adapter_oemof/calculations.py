2836dd6c3f276a0a54c9da85418c9f2989499db5
jnnr <32454596+jnnr@users.noreply.github.com>
2023-02-06 13:18:45 +0100

    Make calculations tolerant to exceptions

--- data_adapter_oemof/calculations.py
+++ data_adapter_oemof/calculations.py
@@ -1,9 +1,31 @@
+import warnings
+
 from oemof.tools.economics import annuity
 
 
+def calculation(func):
+    r"""
+    This is a decorator that allows calculations to fail
+    """
+
+    def decorated_func(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as e:
+            warnings.warn(
+                f"Calculation function '{func.__name__}' \n"
+                f"called with {args, kwargs} \n"
+                f"failed because of: \n" + str(e)
+            )
+
+    return decorated_func
+
+
+@calculation
 def get_name(region, carrier, tech):
     return f"{region}-{carrier}-{tech}"
 
 
+@calculation
 def get_capacity_cost(overnight_cost, fixed_cost, lifetime, wacc):
     return annuity(overnight_cost, lifetime, wacc) + fixed_cost
