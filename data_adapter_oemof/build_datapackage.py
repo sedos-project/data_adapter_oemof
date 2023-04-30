dd9f2574fc7bcd1a365fa4df7cc85938b4e809e4
Julian.Endres <julian.endres@rl-institut.de>
2023-05-01 00:55:15 +0200

    Add draft of time series refactoring
    This is a first draft for the timeseries refactoring.
    Needs to be put into a separate function.

--- data_adapter_oemof/build_datapackage.py
+++ data_adapter_oemof/build_datapackage.py
@@ -4,15 +4,17 @@ from collections import defaultdict
 
 import pandas as pd
 
-import data_adapter.preprocessing as preprocessing
+from data_adapter import preprocessing, core
 from data_adapter_oemof.adapters import TYPE_MAP
-from data_adapter_oemof.mappings import PROCESS_TYPE_MAP
+from data_adapter_oemof.mappings import PROCESS_TYPE_MAP, GLOBAL_PARAMETER_MAP
 from oemof.tabular.datapackage.building import infer_metadata
 
 
 @dataclasses.dataclass
 class datapackage:
-    data: dict  # datadict with scalar data in form of {type:pd.DataFrame(type)}
+    parametrized_elements: dict  # datadict with scalar data in form of {type:pd.DataFrame(type)}
+    parametrized_sequences: dict  # timeseries in form of {type:pd.DataFrame(type)}
+    foreign_keys: dict  # foreign keys for timeseries profiles
     struct: dict
     process_data: dict
 
@@ -25,9 +27,10 @@ class datapackage:
     @staticmethod
     def __refactor_timeseries(timeseries: pd.DataFrame):
         """
-        Takes timeseries in parameter-model format (as a single line entry) And turns into
-        Tabular matching format with index as timeseries timestamps and
-        first column containing data
+        Takes timeseries in parameter-model format (as a single line entry) And
+        turns into Tabular matching format with index as timeseries timestamps
+        and columns containing data
+
         :return: pd.DataFrame:
 
         """
@@ -37,7 +40,8 @@ class datapackage:
         cls, es_structure, **process_data: dict[str, preprocessing.Process]
     ):
         """
-        Adapting the resource scalars for a Datapackage using adapters from adapters.py
+        Adapting the resource scalars for a Datapackage using adapters from
+        adapters.py
 
         :param es_structure:
         :param process_data: preprocessing.Process
@@ -46,6 +50,7 @@ class datapackage:
         parametrized_elements = {}
         struct_io: dict
         parametrized_sequences = {}
+        foreign_keys = {}
         # Todo: Schleife ohne zip bzw. Zip verkürzen
         #  Ohne .apply? Siehe test_data_adapter.test_adapter
         for (process, data), (process, struct_io) in zip(
@@ -55,7 +60,7 @@ class datapackage:
 
             adapter = TYPE_MAP[process_type]
             scalars = data.scalars.apply(
-                adapter.parametrize_dataclass,
+                func=adapter.parametrize_dataclass,
                 struct=struct_io,
                 process_type=process_type,
                 axis=1,
@@ -71,11 +76,86 @@ class datapackage:
                     axis=0, ignore_index=True,
                 )
             else:
-                parametrized_elements[process_type] = pd.DataFrame(
-                    [param.as_dict() for param in scalars.values]
-                )
+                # add new process_type
+                parametrized_elements[process_type] = scalars
+
+            if not data.timeseries.empty:
+
+
+                # def match_sequences_to_scalar():
+                # Add profile column to volatile facade
+                # match timeseries and row
+                # refactor timeseries add name/id
+                # place name/id in profile columns of scalar
+
+                columns = [i for i in data.timeseries.columns if 'timeindex'in i]
+                # TODO column order might not be correct in groupby? hardcode column names?
+                # iterate over different timeindex
+                # timeseries with different timeindex needs to be stores in
+                # separate csv files
+                # naming convention process_year? or process_profile_year?
+                for (start, end, freq), df in data.timeseries.groupby(columns):
+                    timeindex = pd.date_range(start=start,
+                                  end=end,
+                                  freq=pd.Timedelta(
+                                      freq))
+                    # get column names of timeseries only
+                    ts_column = set(data.timeseries.columns).difference(
+                        core.TIMESERIES_COLUMNS.keys())
+                    year = pd.to_datetime(start).year
+
+                    df_timeseries_year = pd.DataFrame()
+                    # iterate over columns in case there are multiple timeseries
+                    # e.g. extraction turbine
+                    for profile_name, profile_column in df.loc[:,
+                                                        ts_column].items():
+                        profile_column = profile_column.dropna()
+                        # unnest timeseries
+                        profile_column = profile_column.explode()
+                        profile_column = profile_column.to_frame()
+                        # add hour of year
+                        profile_column["hour_of_year"] = profile_column.groupby(
+                            axis=0,
+                            level=0).cumcount()
+                        profile_column = profile_column.reset_index().pivot(
+                            columns="index", index="hour_of_year",
+                            values="onshore")
+                        # rename columns to regions
+                        profile_column = profile_column.rename(
+                            columns=df["region"].to_dict())
+                        # add multindex level to column with tech name
+                        # TODO column_name == profile_name should come from
+                        #  links.csv needs to be changed
+                        profile_column.columns = pd.MultiIndex.from_product(
+                            [[profile_name], profile_column.columns])
+                        profile_column.index = timeindex
+                        # append other timeseries of facade e.g. extraction
+                        # turbine has multiple efficiency ts
+                        df_timeseries_year = pd.concat(
+                            [df_timeseries_year, profile_column],
+                            axis=1)
+
+                    # name of csv file
+                    # TODO change process_type to facade_type?!
+                    facade_year = f"{process_type}_{year}"
+                    # check if process_type already exists
+                    if facade_year in parametrized_sequences.keys():
+                        # concat processes to existing
+                        parametrized_sequences[facade_year] = pd.concat(
+                            [parametrized_sequences[facade_year], df_timeseries_year],
+                            axis=1,
+                        )
+                    else:
+                        # add new process_type/facade
+                        parametrized_sequences[facade_year] = df_timeseries_year
+
+
         return cls(
-            data=parametrized_elements, struct=es_structure, process_data=process_data
+            parametrized_elements=parametrized_elements,
+            parametrized_sequences=parametrized_sequences,
+            process_data=process_data,
+            foreign_keys=foreign_keys,
+            struct=es_structure
         )
 
     def get_foreign_keys(self):
