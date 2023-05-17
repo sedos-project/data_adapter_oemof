4f9ed7d4a30bde4e7435d86092d61e6c19f91fa1
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-05-17 16:42:03 +0200

    Add test for timeseries refactoring

--- tests/test_build_datapackage.py
+++ tests/test_build_datapackage.py
@@ -3,7 +3,7 @@ from data_adapter.preprocessing import Adapter
 
 from data_adapter.databus import download_collection
 from utils import PATH_TEST_FILES, PATH_TMP, check_if_csv_dirs_equal
-from data_adapter_oemof.build_datapackage import DataPackage
+from data_adapter_oemof.build_datapackage import DataPackage, refactor_timeseries
 
 
 path_default = (
@@ -13,6 +13,17 @@ path_default = (
     / "data"
     / "elements"
 )
+
+
+def test_refactor_timeseries():
+    adapter = Adapter(
+        "hack-a-thon",
+        structure_name="structure",
+        links_name="links",
+    )
+    process_data = adapter.get_process("modex_tech_wind_turbine_onshore")
+    refactored_ts = refactor_timeseries(process_data.timeseries)
+    print(refactored_ts)
 
 
 def test_build_tabular_datapackage():
