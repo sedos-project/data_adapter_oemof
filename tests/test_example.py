d1a5d32894f7a2d47b158159b50826d20665c985
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-05-17 10:44:04 +0200

    Remove test test

--- tests/test_example.py
+++ tests/test_example.py
@@ -37,16 +37,3 @@ def test_build_tabular_datapackage():
     # FIXME: Get them closer together
     #  - Bus naming with regions -> get regions funktion von Hendrik
     #  - multiple inputs/outputs
-
-
-def test_test():
-    df1 = pd.DataFrame({"a": [1, 2], "b": [3, 4]})
-    df2 = pd.DataFrame({"b": [3.0, 4.0], "a": [1, 2]})
-    pd.testing.assert_frame_equal(
-        df1,
-        df2,
-        check_less_precise=True,
-        check_column_type=False,
-        check_dtype=False,
-        check_like=True,
-    )
