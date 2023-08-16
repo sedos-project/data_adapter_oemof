459fe4b40a3686d83b58cd2f784f1a26c0a5a9e0
FelixMau <fmau@posteo.de>
2023-08-16 13:05:10 +0200

    Pre-commit

--- tests/test_build_datapackage.py
+++ tests/test_build_datapackage.py
@@ -98,10 +98,10 @@ def test_build_datapackage():
                         2: "storage_battery",
                     },
                     "carrier": {
-                               0: "Lithium",
-                               1: "Lithium",
-                               2: "Lithium",
-                           },
+                        0: "Lithium",
+                        1: "Lithium",
+                        2: "Lithium",
+                    },
                     "fixed_costs": {0: 1, 1: 2, 2: 3},
                 }
             )
@@ -131,7 +131,6 @@ def test_build_datapackage():
                         1: "gas",
                         2: "gas",
                     },
-
                     "condensing_efficiency": {0: 0.85, 1: 0.85, 2: 0.9},
                     "electric_efficiency": {0: 0.35, 1: 0.35, 2: 0.4},
                     "thermal_efficiency": {0: 0.5, 1: 0.5, 2: 0.45},
@@ -159,11 +158,11 @@ def test_build_datapackage():
                         1: "wind_turbine_onshore",
                         2: "wind_turbine_onshore",
                     },
-                "carrier": {
+                    "carrier": {
                         0: "wind",
                         1: "wind",
                         2: "wind",
-            }
+                    },
                 }
             )
             process_mock.timeseries = pd.DataFrame(
@@ -202,9 +201,6 @@ def test_build_datapackage():
     mock_adapter.get_process.side_effect = mock_get_process
     # Call the method with the mock adapter
     result = DataPackage.build_datapackage(mock_adapter)
-    result.save_datapackage_to_csv("_files/build_datapackage_test_no_periodic_values")
-
-    result.yearly_scalars_to_periodic_values()
     result.save_datapackage_to_csv("_files/build_datapackage_test")
 
     check_if_csv_dirs_equal(
