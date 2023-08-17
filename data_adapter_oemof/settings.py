a7b7303b502690b5e24fc238ca0fa2fed23ca930
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-08-17 10:11:24 +0200

    Fix collections and structures dir defaults

--- data_adapter_oemof/settings.py
+++ data_adapter_oemof/settings.py
@@ -1,4 +1,27 @@
+import os
 import pathlib
 
 ROOT_DIR = pathlib.Path(__file__).parent
-COLLECTIONS_DIR = ROOT_DIR / "tests" / "collections"
+
+COLLECTIONS_DIR = (
+    pathlib.Path(os.environ["COLLECTIONS_DIR"])
+    if "COLLECTIONS_DIR" in os.environ
+    else pathlib.Path.cwd() / "collections"
+)
+if not COLLECTIONS_DIR.exists():
+    raise FileNotFoundError(
+        f"Could not find collections directory '{COLLECTIONS_DIR}'. "
+        "You should either create the collections folder or "
+        "change path to collection folder by changing environment variable 'COLLECTIONS_DIR'.",
+    )
+STRUCTURES_DIR = (
+    pathlib.Path(os.environ["STRUCTURES_DIR"])
+    if "STRUCTURES_DIR" in os.environ
+    else pathlib.Path.cwd() / "structures"
+)
+if not STRUCTURES_DIR.exists():
+    raise FileNotFoundError(
+        f"Could not find structure directory '{STRUCTURES_DIR}'. "
+        "You should either create the structure folder or "
+        "change path to structure folder by changing environment variable 'STRUCTURES_DIR'.",
+    )
