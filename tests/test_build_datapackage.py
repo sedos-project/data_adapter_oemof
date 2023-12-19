19c1e815bfa5bc2395c48a7f17a13b1649dcb1b3
FelixMau <fmau@posteo.de>
2023-12-19 13:37:55 +0100

    Linting

--- tests/test_build_datapackage.py
+++ tests/test_build_datapackage.py
@@ -48,8 +48,11 @@ def test_build_tabular_datapackage_from_adapter():
     download_collection(
         "https://databus.openenergyplatform.org/felixmaur/collections/hack-a-thon/"
     )
-    structure = Structure("SEDOS_Modellstruktur", process_sheet="hack-a-thon",
-                          parameter_sheet="Parameter_IO_hack-a-thon")
+    structure = Structure(
+        "SEDOS_Modellstruktur",
+        process_sheet="hack-a-thon",
+        parameter_sheet="Parameter_IO_hack-a-thon",
+    )
 
     adapter = Adapter(
         "hack-a-thon",
@@ -103,7 +106,7 @@ def test_build_tabular_datapackage_from_adapter():
     # FIXME: Demand is in different Format than expected.
 
 
-#@pytest.mark.skip(reason="Pumped storage data has no lifetime yet")
+@pytest.mark.skip(reason="Pumped storage data has no lifetime yet")
 def test_read_datapackage():
     es = EnergySystem.from_datapackage(
         os.path.join(
