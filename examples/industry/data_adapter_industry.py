b98074dd24fc07e69a994c923554589fe2309828
FelixMau <fmau@posteo.de>
2024-07-30 16:15:58 +0200

    Extend `parameter_name` mapping

--- examples/industry/data_adapter_industry.py
+++ examples/industry/data_adapter_industry.py
@@ -13,7 +13,7 @@ from oemof.tabular.datapackage.reading import (
     deserialize_constraints,
     deserialize_energy_system,
 )
-from oemof.tabular.facades import Commodity, Conversion, Excess, Load, Volatile
+from oemof.tabular.facades import Commodity, Conversion, Excess, Load, Volatile, Storage
 from oemof_industry.mimo_converter import MIMO
 
 from data_adapter_oemof.build_datapackage import DataPackage  # noqa: E402
@@ -77,7 +77,7 @@ process_adapter_map = pd.concat(
 ).to_dict(orient="dict")["facade adapter (oemof)"]
 
 parameter_map = {
-    "DEFAULT": {},
+    "DEFAULT": {"interest_rate":"wacc"},
     "StorageAdapter": {
         "capacity_potential": "expansion_limit",
         "capacity": "installed_capacity",
@@ -85,12 +85,202 @@ parameter_map = {
         "inflow_conversion_factor": "input_ratio",
         "outflow_conversion_factor": "output_ratio",
     },
-    "MIMOAdapter": {
-        "capacity_cost": "cost_fix_capacity_w",
-        "capacity": "capacity_w_resid",
-        "expandable": "capacity_w_abs_new_max",
+    "CommodityAdapter": {
     },
-    "modex_tech_wind_turbine_onshore": {"profile": "onshore"},
+    "x2x_import_biogas":{"amount":"capacity_w_inst_0",
+                         "marginal_cost":"cost_var_e"},
+    "x2x_import_coal":{"amount":"capacity_w_inst_0",
+                         "marginal_cost":"cost_var_e"},
+    "x2x_import_hydrogen_renewable":{"amount":"capacity_w_inst_0",
+                         "marginal_cost":"cost_var_e"},
+    "x2x_other_biogas_treatment":{"marginal_cost":"cost_var_e"},
+
+
+
+
+    "ind_source_steel_scrap_iron":{"amount":"capacity_w_inst_0"},
+    "ind_steel_sinter_0": {"capacity":"capacity_w_inst_0"},
+    "ind_steel_coke_plant_0": {"capacity":"capacity_e_inst_0"},
+    "ind_steel_blafu_0": {"capacity":"capacity_w_inst_0"},
+    "ind_steel_oxyfu_0": {"capacity":"capacity_w_inst_0"},
+    "ind_steel_casting_0": {"capacity":"capacity_w_inst_0"},
+    "ind_steel_elefu_0": {"capacity":"capacity_w_inst_0"},
+    "x2x_delivery_methane_pipeline_0": {
+                                "capacity":"capacity_p_max",
+                                    "marginal_cost":"cost_var_p",
+    },
+    "x2x_x2gas_sr_syngas_0": {"capacity":"capacity_p_inst",
+                               "capacity_cost": "cost_inv_p",
+                               "fixed_costs":"cost_fix_p",
+    },
+
+
+    "ind_steel_coke_plant_1": {"capacity_potential":"capacity_e_abs_new_max",
+                               "capacity_cost": "cost_inv_e",
+                               "fixed_costs":"cost_fix_e",
+                               "marginal_cost":"cost_var_e"
+    },
+    "ind_steel_sinter_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w"
+    },
+    "ind_steel_blafu_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w"
+    },
+    "ind_steel_blafu_cc_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w"
+    },
+    "ind_steel_oxyfu_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w",
+                                #"max":"availability_constant"
+    },
+    "ind_steel_casting_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w",
+                                #"max":"availability_constant"
+    },
+    "ind_steel_elefu_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w",
+                                #"max":"availability_constant"
+    },
+    "ind_steel_dirred_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w",
+                                #"max":"availability_constant"
+    },
+    "ind_steel_sponge_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w",
+                                #"max":"availability_constant"
+    },
+    "ind_steel_pellet_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w",
+                                #"max":"availability_constant"
+    },
+    "ind_steel_hyddri_1": {"capacity_potential":"capacity_w_abs_new_max",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+                                "marginal_cost":"cost_var_w",
+                                #"max":"availability_constant"
+    },
+   "x2x_delivery_hydrogen_pipeline_retrofit_1": {"capacity_potential":"capacity_p_max",
+                               "capacity_cost": "cost_inv_p",
+                               "fixed_costs":"cost_fix_p",
+                                "marginal_cost":"cost_var_p",
+    },
+    "x2x_delivery_hydrogen_pipeline_new_1": {
+                               "capacity_cost": "cost_inv_p",
+                               "fixed_costs":"cost_fix_p",
+    },
+    "x2x_g2p_pemfc_ls_1": {
+                               "capacity_cost": "cost_inv_p",
+                               "fixed_costs":"cost_fix_p",
+    },
+    "x2x_g2p_sofc_ls_1": {
+                               "capacity_cost": "cost_inv_p",
+                               "fixed_costs":"cost_fix_p",
+    },
+    "x2x_other_dac_ht_1": {     "marginal_cost":"cost_var_w",
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_other_dac_lt_1": {
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_p2gas_aec_1": {
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_p2gas_biom_1": {
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_p2gas_pemec_1": {
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_p2gas_biom_1": {
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_p2gas_pemec_1": {
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_p2gas_sabm_1": {
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_p2gas_soec_1": {
+                               "capacity_cost": "cost_inv_w",
+                               "fixed_costs":"cost_fix_w",
+    },
+    "x2x_x2gas_mpyr_1": {
+                               "capacity_cost": "cost_inv_p",
+                               "fixed_costs":"cost_fix_p",
+                                "marginal_cost":"cost_var_e"
+    },
+    "x2x_x2gas_sr_syngas_1": {
+                               "capacity_cost": "cost_inv_p",
+                               "fixed_costs":"cost_fix_p",
+    },
+
+
+    "x2x_storage_hydrogen_lohc_1": {
+                               "efficiency": "efficiency_sto_in",
+                               "fixed_costs":"cost_fix_w",
+                                "loss_rate":"sto_self_discharge",
+                                "storage_capacity_cost":"cost_inv_e",
+                                "fixed_costs":"cost_fix_p",
+                                "marginal_cost":"cost_var_e"
+    },
+    "x2x_storage_hydrogen_new_1": {
+                               "efficiency": "efficiency_sto_in",
+                               "fixed_costs":"cost_fix_w",
+                                "loss_rate":"sto_self_discharge",
+                                "storage_capacity_cost":"cost_inv_e",
+                                "fixed_costs":"cost_fix_p",
+                                "marginal_cost":"cost_var_e",
+                                "capacity_capacity_potential":"capacity_e_max"
+    },
+    "x2x_storage_hydrogen_retrofit_1": {
+                               "efficiency": "efficiency_sto_in",
+                               "fixed_costs":"cost_fix_p",
+                                "loss_rate":"sto_self_discharge",
+                                "storage_capacity_cost":"cost_inv_e",
+                                "fixed_costs":"cost_fix_p",
+                                "marginal_cost":"cost_var_e",
+                                "capacity_capacity_potential":"capacity_e_max",
+                                "storage_capacity":"capacity_e_inst"
+    },
+    "x2x_storage_methane_0": {
+                               "efficiency": "efficiency_sto_in",
+                               "fixed_costs":"cost_fix_p",
+                                "loss_rate":"sto_self_discharge",
+                                "storage_capacity_cost":"cost_inv_e",
+                                "fixed_costs":"cost_fix_p",
+                                "marginal_cost":"cost_var_e",
+                                "capacity_capacity_potential":"capacity_e_max",
+                                "storage_capacity":"capacity_e_inst"
+    },
+
+
+
 }
 logger.info("Building datapackage...")
 dp = DataPackage.build_datapackage(
@@ -103,7 +293,6 @@ dp.save_datapackage_to_csv(str(datapackage_path))
 
 
 logger.info("Building EnergySystem")
-
 es = EnergySystem.from_datapackage(
     path="datapackage/datapackage.json",
     typemap={
@@ -114,6 +303,7 @@ es = EnergySystem.from_datapackage(
         "load": Load,
         "volatile": Volatile,
         "mimo": MIMO,
+        "storage":Storage
     },
 )
 
